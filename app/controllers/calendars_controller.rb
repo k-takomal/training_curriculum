1a8f042f2f0f6456c07843103bf42be3cc406e89
k-takomal <foxinn009@gmail.com>
2020-12-23 04:14:11 +0900

    issue6修正、追加

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -25,6 +25,9 @@ class CalendarsController < ApplicationController
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
+    #今日の曜日を@wdayに代入
+    @wday = Date.today.wday
+
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
@@ -34,11 +37,14 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = x
+
+      #wdaysから値を取り出すための数値を取得
+      wday_num = @wday + x
       if wday_num >= 7
         wday_num = wday_num -7
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num]}
+
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
 
       @week_days.push(days)
     end
